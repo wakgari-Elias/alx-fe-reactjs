8d327fc69a220fcfcbef5c0fe7235dd7a1cb101e
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-08-11 21:33:23 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from './WelcomeMessage';
 import Header from './Header';,
 import MainContent from './MainContent';, 
 import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -24,6 +25,11 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />  
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
       <Footer />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
