f0d45713ebaeefc767e009e5c442434171c72b6d
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-08-18 06:04:37 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header';,
 import MainContent from './MainContent';, 
 import Footer from './Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
