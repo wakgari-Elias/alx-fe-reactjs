69566d4cf4002b04843d19e7b3e0abbffa71485e
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-08-18 06:00:02 +0300

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,4 +1,7 @@
-<footer>
-  <p>© 2023 City Lovers</p>
+<footer style={{ backgroundColor: 'lightgrey', padding: '20px', border: '1px solid #ddd', textAlign: 'center' }}>
+  <p style={{ color: 'darkblue', fontSize: '18px' }}>
+    © 2023 City Lovers
+  </p>
 </footer>
+
 export default Footer;
