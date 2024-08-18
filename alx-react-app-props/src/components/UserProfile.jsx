a7d869e465e6dbef61601832cde9579f73f12163
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-08-18 23:38:40 +0300

    Update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react';
+import UserContext from '../UserContext'; // Adjust the path if necessary
 
-const UserProfile = (props) => {
+const UserProfile = () => {
+  const userData = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
     </div>
   );
 };
