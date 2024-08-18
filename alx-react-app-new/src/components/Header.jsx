f95c6bc444b5fb8a14cb8fc5f51e169884b91249
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-08-18 05:56:18 +0300

    Update Header.jsx

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,4 +1,5 @@
-<header>
-  <h1>My Favorite Cities</h1>
-</header>
+
+<header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+   <h1>My Favorite Cities</h1>
+ </header>
 export default Header;
