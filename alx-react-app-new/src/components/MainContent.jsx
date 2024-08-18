521fc7344616784979b7f1ecc39ba79e2c803c44
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-08-18 05:59:10 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,5 +1,7 @@
 
-<main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>
+<main style={{ backgroundColor: 'lightgrey', padding: '20px', border: '1px solid #ddd' }}>
+        <p style={{ color: 'darkblue', fontSize: '18px', textAlign: 'center' }}>
+          I love to visit New York, Paris, and Tokyo.
+        </p>
+      </main>
 export default  MainContent;
