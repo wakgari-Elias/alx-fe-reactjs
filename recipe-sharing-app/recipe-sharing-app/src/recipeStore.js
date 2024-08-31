95320441f1a5b4757491150d362e96c6b7ce64e8
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-09-01 00:03:39 +0300

    Update recipeStore.js

--- recipe-sharing-app/recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/recipe-sharing-app/src/recipeStore.js
@@ -1,1 +1,12 @@
 
+import create from 'zustand';
+
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({
+    recipes: [...state.recipes, newRecipe]
+  })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
