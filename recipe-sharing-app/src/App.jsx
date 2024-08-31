87082df321969981c14228c22d30ce1ce02855fd
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-09-01 01:39:43 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails'; // Ensure this component exists
 import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (
-    <div>
-      <SearchBar />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+          {/* Add other routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
