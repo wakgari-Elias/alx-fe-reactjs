b6070443e88dd3c316737058a86a9bb4adb1d082
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-09-07 23:17:39 +0300

    Create BlogPost.jsx

---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BlogPost = () => {
+  return (
+    <>
+        <div>
+            <h2>This is a Blog Post</h2>
+        </div>
+    </>
+  )
+}
+
+export default BlogPost;
