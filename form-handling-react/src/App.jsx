6a9257eb6750e8a887491da2e1df59468543e5e8
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-09-07 21:11:43 +0300

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css';
+// src/App.js
+import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>User Registration</h1>
+    <div>
+      <h1>Controlled Form</h1>
       <RegistrationForm />
+      <h1>Formik Form</h1>
+      <FormikForm />
     </div>
   );
-}
+};
 
 export default App;
