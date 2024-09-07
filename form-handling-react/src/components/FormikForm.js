37702f0358433aee2e63c27cc5e205c3363d6ab1
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-09-07 21:19:43 +0300

    Update FormikForm.js

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -1,48 +1,68 @@
-// src/components/FormikForm.js
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-const validationSchema = Yup.object({
-  username: Yup.string().required('Username is required'),
-  email: Yup.string().email('Invalid email address').required('Email is required'),
-  password: Yup.string().required('Password is required'),
-});
-
 const FormikForm = () => {
-  return (
-    <Formik
-      initialValues={{ username: '', email: '', password: '' }}
-      validationSchema={validationSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log('Form submitted', values);
-        setSubmitting(false);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <div>
-            <label>Username:</label>
-            <Field type="text" name="username" />
-            <ErrorMessage name="username" component="span" />
-          </div>
-          <div>
-            <label>Email:</label>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="span" />
-          </div>
-          <div>
-            <label>Password:</label>
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="span" />
-          </div>
-          <button type="submit" disabled={isSubmitting}>
-            Register
-          </button>
-        </Form>
-      )}
-    </Formik>
-  );
+    const initialValues = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    };
+
+    const validationSchema = Yup.object({
+        firstName: Yup.string().required('First Name is required'),
+        lastName: Yup.string().required('Last Name is required'),
+        email: Yup.string().email('Invalid email address').required('Email is required'),
+        password: Yup.string().required('Password is required'),
+        confirmPassword: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Passwords must match')
+            .required('Confirm Password is required'),
+    });
+
+    const handleSubmit = (values) => {
+        // Handle form submission here
+        console.log(values);
+    };
+
+    return (
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+            <Form>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <Field type="text" id="firstName" name="firstName" />
+                    <ErrorMessage name="firstName" component="div" />
+                </div>
+
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <Field type="text" id="lastName" name="lastName" />
+                    <ErrorMessage name="lastName" component="div" />
+                </div>
+
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <Field type="email" id="email" name="email" />
+                    <ErrorMessage name="email" component="div" />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <Field type="password" id="password" name="password" />
+                    <ErrorMessage name="password" component="div" />
+                </div>
+
+                <div>
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <Field type="password" id="confirmPassword" name="confirmPassword" />
+                    <ErrorMessage name="confirmPassword" component="div" />
+                </div>
+
+                <button type="submit">Submit</button>
+            </Form>
+        </Formik>
+    );
 };
 
 export default FormikForm;
