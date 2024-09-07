9e5b14193979d249adddeae79089cbc01b2b539f
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-09-07 20:53:02 +0300

    Create FormikForm.js

---
+++ form-handling-react/src/components/FormikForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const FormikForm = () => {
+  const initialValues = { username: '', email: '', password: '' };
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required('Username is required'),
+    email: Yup.string().email('Invalid email format').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const onSubmit = (values) => {
+    // Simulate an API call
+    console.log(values);
+    alert('Registration successful');
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div>
+          <label>Username:</label>
+          <Field type="text" name="username" />
+          <ErrorMessage name="username" component="div" />
+        </div>
+        <div>
+          <label>Email:</label>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+        </div>
+        <div>
+          <label>Password:</label>
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+        </div>
+        <button type="submit">Register</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default FormikForm;
