b6ce15caccdced7d33c583740ce86586ca72afbe
Elias wakgari <154916266+wakgari-Elias@users.noreply.github.com>
2024-09-07 21:11:08 +0300

    Update FormikForm.js

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -1,4 +1,4 @@
-// src/components/FormikForm.jsx
+// src/components/FormikForm.js
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
@@ -10,35 +10,37 @@ const validationSchema = Yup.object({
 });
 
 const FormikForm = () => {
-  const handleSubmit = (values) => {
-    // Simulate API call
-    console.log('Form Submitted:', values);
-  };
-
   return (
     <Formik
       initialValues={{ username: '', email: '', password: '' }}
       validationSchema={validationSchema}
-      onSubmit={handleSubmit}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log('Form submitted', values);
+        setSubmitting(false);
+      }}
     >
-      <Form>
-        <div>
-          <label>Username</label>
-          <Field type="text" name="username" />
-          <ErrorMessage name="username" component="p" />
-        </div>
-        <div>
-          <label>Email</label>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="p" />
-        </div>
-        <div>
-          <label>Password</label>
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="p" />
-        </div>
-        <button type="submit">Register</button>
-      </Form>
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <label>Username:</label>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="span" />
+          </div>
+          <div>
+            <label>Email:</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="span" />
+          </div>
+          <div>
+            <label>Password:</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="span" />
+          </div>
+          <button type="submit" disabled={isSubmitting}>
+            Register
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
